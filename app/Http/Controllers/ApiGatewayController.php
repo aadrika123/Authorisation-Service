c4d4f58f25699c0841e64369f330b80498b5ed84
mrinal9911 <mrinalk123456@gmail.com>
2023-07-31 12:24:51 +0530

    api gateway conytroller

--- app/Http/Controllers/ApiGatewayController.php
+++ app/Http/Controllers/ApiGatewayController.php
@@ -119,7 +119,6 @@ class ApiGatewayController extends Controller
             $response = $response->$method($url . $req->getRequestUri(), ($fileName ? $new2 : $new));
             if (isset(json_decode($response)->status)) {
                 if (json_decode($response)->status == false) {
-                    // return responseMsg(false, json_decode($response)->message, []);
                     return json_decode($response);
                 }
                 return json_decode($response);
@@ -158,5 +157,105 @@ class ApiGatewayController extends Controller
             }
         }
         return $result;
+    }
+
+    public function unAuthApis($req)
+    {
+        try {
+            // Converting environmental variables to Services
+            $baseURLs = Config::get('constants.MICROSERVICES_APIS');
+            $services = json_decode($baseURLs, true);
+            // Sending to Microservices
+            $segments = explode('/', $req->path());
+            $service = $segments[1];
+            if (!array_key_exists($service, $services))
+                throw new Exception("Service Not Available");
+
+            $url = $services[$service];
+            $bearerToken = (collect(($req->headers->all())['authorization'] ?? "")->first());
+            $contentType = (collect(($req->headers->all())['content-type'] ?? "")->first());
+            $ipAddress = getClientIpAddress();
+            $method = $req->method();
+            if ($segments[1] == "trade" && strtolower($req->getMethod()) == "get") {
+            } else {
+                $req = $req->merge([
+                    // 'auth' => authUser(),
+                    // 'token' => $req->bearerToken(),
+                    // 'currentAccessToken' => $req->user()->currentAccessToken(),
+                    // 'apiToken' => $req->user()->currentAccessToken()->token,
+                    "authReq" => false,
+                    'ipAddress' => $ipAddress
+                ]);
+            }
+            #======================
+
+            $response = Http::withHeaders(
+                [
+                    "Authorization" => "Bearer $bearerToken",
+                    'API-KEY' => collect($req->headers)->toArray()['api-key'] ?? "",
+                ]
+            );
+            $fileName = [];
+            $new = [];
+            foreach ($_FILES as $index => $val) {
+                array_push($fileName, $index);
+            }
+
+            foreach (collect($req->all())->toArray() as $key => $val) {
+                $new[$key] = $val;
+            }
+            $dotIndexes = $this->generateDotIndexes($_FILES);
+
+            foreach ($dotIndexes as $val) {
+                $patern = "/\.name/i";
+                if (!preg_match($patern, $val)) {
+                    continue;
+                }
+                $name = "";
+                $test = collect(explode(".", preg_replace($patern, "", $val)));
+                $t = $test->filter(function ($val, $index) {
+                    return $index > 0 ? true : "";
+                });
+                $t = $t->map(function ($val) {
+                    return "[" . $val . "]";
+                });
+                $name = (($test[0]) . implode("", $t->toArray()));
+                $response = $response->attach(
+                    $name,
+                    file_get_contents($this->getArrayValueByDotNotation($_FILES, preg_replace($patern, ".tmp_name", $val))),
+                    $this->getArrayValueByDotNotation($_FILES, $val)
+                );
+            }
+            $textIndex = $this->generateDotIndexes($new);
+            $new2 = [];
+            foreach ($textIndex as $val) {
+                $name = "";
+                $test = collect(explode(".", $val));
+                $t = $test->filter(function ($val, $index) {
+                    return $index > 0 ? true : "";
+                });
+                $t = $t->map(function ($val) {
+                    return "[" . $val . "]";
+                });
+                $name = (($test[0]) . implode("", $t->toArray()));
+                $new2[] = [
+                    "contents" => $this->getArrayValueByDotNotation($new, $val),
+                    "name" => $name
+                ];
+            }
+
+            # Check if the response is valid to retirn in json format 
+            $response = $response->$method($url . $req->getRequestUri(), ($fileName ? $new2 : $new));
+            if (isset(json_decode($response)->status)) {
+                if (json_decode($response)->status == false) {
+                    return json_decode($response);
+                }
+                return json_decode($response);
+            } else {
+                return $response;
+            }
+        } catch (Exception $e) {
+            return responseMsgs(false, $e->getMessage(), "");
+        }
     }
 }
