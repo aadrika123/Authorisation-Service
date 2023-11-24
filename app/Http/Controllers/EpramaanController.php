f80646a1c955b62166aca271956bc8966a271404
mrinal9911 <mrinalk123456@gmail.com>
2023-11-24 16:43:50 +0530

    push

--- app/Http/Controllers/EpramaanController.php
+++ app/Http/Controllers/EpramaanController.php
@@ -182,7 +182,7 @@ class EpramaanController extends Controller
         // JWS Verifier.
         $jwsVerifier = new JWSVerifier($algorithmManager);
         $key = JWKFactory::createFromCertificateFile(
-            'home/certificate/epramaan.crt', // The path where the certificate has been stored
+            'http//:192.168.0.95/home/certificate/epramaan.crt', // The path where the certificate has been stored
             [
                 'use' => 'sig', // Additional parameters
             ]
