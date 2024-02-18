c007e1bd481b81dd4dcb72d71e42b9db2077deb6
mrinal9911 <mrinalk123456@gmail.com>
2024-02-18 12:57:47 +0530

    commit

--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,7 @@ Route::controller(UserController::class)->group(function () {
     Route::post('login', 'loginAuth');                                                        #API_ID = 4161
     Route::post('register', 'store');
     Route::post('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('get-user-dtl', 'userDtls')->middleware('auth:sanctum');
 });
 
 /**
