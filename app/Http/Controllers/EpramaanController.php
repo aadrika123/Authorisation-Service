d4f42e50684a0b2a690145f99b8e9b1e55ef1421
Arsh620 <arshadhussain82987@gmail.com>
2025-01-06 15:50:38 +0530

    new chmmxdee

--- app/Http/Controllers/EpramaanController.php
+++ app/Http/Controllers/EpramaanController.php
@@ -592,7 +592,7 @@ class EpramaanController extends Controller
             // Step 6: Generate HMAC hash
 
             $hmac = hash_hmac('sha256', $inputValue, $aesKey);
-            // $hmac = base64_encode($hmac);
+            $hmac = base64_encode($hmac);
 
 
             // Step 7: Prepare data to send
