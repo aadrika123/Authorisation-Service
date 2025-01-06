643979675851a65312e449aa44e2ba6edc81aa78
Arsh620 <arshadhussain82987@gmail.com>
2025-01-06 14:32:18 +0530

    new

--- app/Http/Controllers/EpramaanController.php
+++ app/Http/Controllers/EpramaanController.php
@@ -614,7 +614,8 @@ class EpramaanController extends Controller
 
         // Step 8: Send POST request to the external endpoint
 
-        $url = 'https://epstg.meripehchaan.gov.in/openid/jwt/processOIDCSLORequest.do';
+        // $url = 'https://epstg.meripehchaan.gov.in/openid/jwt/processOIDCSLORequest.do';
+        $url = 'https://epramaan.meripehchaan.gov.in/openid/jwt/processJwtTokenRequest.do';
 
 
 
