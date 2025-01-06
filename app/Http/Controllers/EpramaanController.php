4acdba9679ef76a8693286f59b1908e1c39e95b2
Arsh620 <arshadhussain82987@gmail.com>
2025-01-06 15:49:38 +0530

    new chmmxd

--- app/Http/Controllers/EpramaanController.php
+++ app/Http/Controllers/EpramaanController.php
@@ -591,8 +591,8 @@ class EpramaanController extends Controller
 
             // Step 6: Generate HMAC hash
 
-            $hmac = hash_hmac('sha256', $inputValue, $aesKey,true);
-            $hmac = base64_encode($hmac);
+            $hmac = hash_hmac('sha256', $inputValue, $aesKey);
+            // $hmac = base64_encode($hmac);
 
 
             // Step 7: Prepare data to send
@@ -636,7 +636,9 @@ class EpramaanController extends Controller
 
             if ($response->successful()) {
 
-                return redirect()->away($redirectUrl); // Redirect to the provided URL
+                // return redirect()->away($redirectUrl); // Redirect to the provided URL
+                return response()->json(['response' => $response], 200); 
+
 
 
             }
