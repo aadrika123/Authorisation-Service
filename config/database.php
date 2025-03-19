95a07cbdf88da8b0ff9e0cf6fbadfafc8ffac93d
Arsh620 <arshadhussain82987@gmail.com>
2025-03-19 10:47:11 +0530

    changes

--- config/database.php
+++ config/database.php
@@ -76,9 +76,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-            'options'  => [
-                PDO::ATTR_EMULATE_PREPARES => true, // :white_check_mark: Required for PgBouncer in transaction mode
-            ],
+            // 'options'  => [
+            //     PDO::ATTR_EMULATE_PREPARES => true, 
+            // ],
         ],
 
         'sqlsrv' => [
