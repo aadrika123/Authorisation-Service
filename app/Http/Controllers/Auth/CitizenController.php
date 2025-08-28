5b1b3263d7dc2ff97f9f99287763c60e1d2265b5
rohan8883 <rohansingh9135@gmail.com>
2025-08-28 13:34:09 +0530

    changed filter

--- app/Http/Controllers/Auth/CitizenController.php
+++ app/Http/Controllers/Auth/CitizenController.php
@@ -337,7 +337,7 @@ class CitizenController extends Controller
             $records = ActiveCitizenUndercare::select(
                     'active_citizens.id',
                     'active_citizens.user_name',
-                    DB::raw('DATE(active_citizens.created_at) as created_date')
+                    DB::raw('DATE(active_citizen_undercares.created_at) as created_date')
                 )
                 ->join('active_citizens', 'active_citizens.id', '=', 'active_citizen_undercares.citizen_id')
                 ->where("active_citizen_undercares.$column", $validated['recordId'])
