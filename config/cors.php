35e4e49da5a4a5ef46aa78ed0649550ab4d38513
Arsh620 <arshadhussain82987@gmail.com>
2025-10-09 11:05:57 +0530

    changes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+     'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'https://www.jharkhandegovernance.com')),
 
     'allowed_origins_patterns' => [],
 
