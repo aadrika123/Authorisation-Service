06dda23643fbaf15bbc3a14f54c5d47b6fb534d0
Arsh620 <arshadhussain82987@gmail.com>
2025-10-24 16:48:54 +0530

    changes

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // protected $proxies;
+    // FOR ACCOUNT LOCK OUT SYSTEM BY IP ADDRESS
+     protected $proxies = '*'; // ✅ Trust all proxies (Nginx, Cloudflare, etc.)
 
     /**
      * The headers that should be used to detect proxies.
